72edaa2c672002c9006bb5d7d050e70a01f94215
Frédéric Boyer <fboyer@gmail.com>
2014-05-21 14:55:17 -0400

    Added solution to problem 5

---
+++ go/p5.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+const (
+	limit = 20
+)
+
+func main() {
+	fmt.Printf("Smallest multiple: %v", findSmallestMultiple())
+}
+
+func findSmallestMultiple() int {
+	// Multiple must be at least equal to the limit
+	for i := limit; ; i++ {
+		for j := limit; j > 0; j-- {
+			// As soon as we find a number that is not divisible we stop the inner loop
+			if i%j != 0 {
+				break
+			}
+			// If we passed all tests then we return the value found
+			if j == 1 {
+				return i
+			}
+		}
+	}
+}
